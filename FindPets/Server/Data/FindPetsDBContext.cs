178638c6d51e460ffbb5f73c9cc2b92a0f7665d5
Marcelo de Moraes Andrade <47828310+MM-Andrade@users.noreply.github.com>
2023-06-04 11:22:38 -0300

    :wrench: feat EFCore Configuration

---
+++ FindPets/Server/Data/FindPetsDBContext.cs
@@ -0,0 +1,14 @@
+﻿using FindPets.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace FindPets.Server.Data
+{
+    public class FindPetsDBContext : DbContext
+    {
+        public FindPetsDBContext(DbContextOptions<FindPetsDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Pets> Pets { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
